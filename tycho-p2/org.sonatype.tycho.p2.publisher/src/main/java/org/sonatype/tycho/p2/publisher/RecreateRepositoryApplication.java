73f4e46572f5d548372494c753617e3e37878b22
Hugues Malphettes <hmalphettes@gmail.com>
2010-06-01 08:55:55 -0700

    prepare work for the signing plugin: application that recreates the artifact repository to update the checksums and take care of the packed artifacts

---
+++ tycho-p2/org.sonatype.tycho.p2.publisher/src/main/java/org/sonatype/tycho/p2/publisher/RecreateRepositoryApplication.java
@@ -0,0 +1,73 @@
+package org.sonatype.tycho.p2.publisher;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Map;
+
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.core.runtime.URIUtil;
+import org.eclipse.equinox.app.IApplication;
+import org.eclipse.equinox.app.IApplicationContext;
+import org.eclipse.equinox.internal.p2.repository.helpers.RepositoryHelper;
+import org.eclipse.equinox.p2.internal.repository.tools.RepositoryDescriptor;
+
+/**
+ * This application is able to fix the checksums after a pack and sign has been run
+ * and generate the descriptors for the pack.gz files.
+ * 
+ * It expects a single argument: -artifactRepository
+ * 
+ */
+public class RecreateRepositoryApplication extends org.eclipse.equinox.p2.internal.repository.tools.RecreateRepositoryApplication
+implements IApplication {
+
+	public Object start(IApplicationContext context) throws Exception {
+		Map<?, ?> args = context.getArguments();
+		initializeFromArguments((String[]) args.get(IApplicationContext.APPLICATION_ARGS));
+		run(new NullProgressMonitor());
+		return IApplication.EXIT_OK;
+	}
+
+	public void stop() {
+		//nothing to do.
+	}
+	
+	public void initializeFromArguments(String[] args) throws Exception {
+		if (args == null)
+		{
+			throw new IllegalArgumentException("The argument -artifactRepository is mandatory");
+		}
+
+		RepositoryDescriptor sourceRepo = new RepositoryDescriptor();
+		sourceRepo.setKind(RepositoryDescriptor.KIND_ARTIFACT);
+
+		boolean sourceRepoSet = false;
+		for (int i = 0; i < args.length; i++) {
+			if (i == args.length - 1 || args[i + 1].startsWith("-")) //$NON-NLS-1$
+				continue;
+
+			String arg = args[++i];
+
+			try
+			{
+				if (args[i - 1].equalsIgnoreCase("-artifactRepository") 
+						|| args[i - 1].equalsIgnoreCase("-a"))
+				{ //$NON-NLS-1$
+					URI uri = RepositoryHelper.localRepoURIHelper(URIUtil.fromString(arg));
+					sourceRepo.setLocation(uri);
+					super.setArtifactRepository(sourceRepo);
+				}
+			} catch (URISyntaxException e) {
+				throw new IllegalArgumentException("invalid URL: " + arg);
+			}
+		}
+		if (!sourceRepoSet)
+		{
+			throw new IllegalArgumentException("The argument -artifactRepository is mandatory");
+		}
+	}
+
+
+	
+	
+}
---
+++ tycho-p2/tycho-p2-publisher-director/src/main/java/org/sonatype/tycho/plugins/p2/tools/FixCheckSumMojo.java
@@ -0,0 +1,38 @@
+package org.sonatype.tycho.plugins.p2.tools;
+
+import java.io.IOException;
+
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.codehaus.plexus.util.cli.Commandline;
+import org.sonatype.tycho.plugins.p2.AbstractP2AppInvokerMojo;
+
+/**
+ * Invoke the application RecreateRepositoryApplication
+ * @goal fix-checksums
+ */
+public class FixCheckSumMojo extends AbstractP2AppInvokerMojo {
+	
+	public static String PUBLISHER_BUNDLE_ID = "org.sonatype.tycho.p2.publisher";
+	public static String RECREATE_REPOSITORY_APPLICATION = PUBLISHER_BUNDLE_ID + ".RecreateRepository";
+	
+	public void execute() throws MojoExecutionException, MojoFailureException {
+		
+		try
+		{
+			Commandline cli = super.getCommandLine(RECREATE_REPOSITORY_APPLICATION);
+			cli.addArguments(new String[] {"-artifactRepository", targetRepository.toURI().toURL().toExternalForm()});
+			super.execute(cli, null);
+		}
+		catch (IOException ioe)
+		{
+			throw new MojoExecutionException("Error invoking the RecreateRepository application", ioe);
+		}
+		
+	}
+
+	
+	
+	
+	
+}
